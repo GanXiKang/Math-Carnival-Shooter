d8f378cc00af2bd086cedf4731944a9b06cd6d75
QiqiLuoluo <gan0612xk@hotmail.com>
2025-11-04 16:40:19 +0800

    feat ： 文件處理

---
+++ Assets/Scripts/Math.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Math", menuName = "Scriptable Objects/Math")]
+public class Math : ScriptableObject
+{
+    public string questionText;
+    public int correctAnswer;
+    public int[] options;
+    public string level;
+}
